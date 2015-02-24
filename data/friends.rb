88748e6d1227d3b2728f9dad00cca26456a10f5b
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-24 15:26:11 -0700

    updated birthdays

--- data/friends.rb
+++ data/friends.rb
@@ -3,13 +3,13 @@ FRIENDS = [
     first_name: "Bobby",
     last_name: "Wilson",
     status: "Married",
-    start_date: Date.new(1984, 9, 11),
+    birth_date: Date.new(1984, 9, 11),
   },
   {
     first_name: "Zach",
     last_name: "Klabunde",
     status: "Single and ready to Mingle",
-    start_date: Date.new(1988, 10, 16),
+    birth_date: Date.new(1988, 10, 16),
   },
   {
     first_name: "Shelby",
